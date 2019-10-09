bacc9f9bbc773eb252f9288c5756c2502268ee80
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2019-10-09 12:14:02 -0400

    adds answer for q2

--- Pursuit-Core-iOS-Bubble-Sort-Lab/Pursuit-Core-iOS-Bubble-Sort-Lab/BubbleSortQuestions.swift
+++ Pursuit-Core-iOS-Bubble-Sort-Lab/Pursuit-Core-iOS-Bubble-Sort-Lab/BubbleSortQuestions.swift
@@ -24,7 +24,28 @@ func bubbleSort<T: Comparable>(arr: [T], by isSorted: (T, T) -> Bool) -> [T] {
 // Sort an array of Ints without changing the position of any negative numbers
 // https://www.geeksforgeeks.org/sort-an-array-without-changing-position-of-negative-numbers/
 
-func bubbleSortWithoutMovingNegatives<T: Comparable>(arr: [T], by isSorted: (T, T) -> Bool) -> [T] {
+func bubbleSortWithoutMovingNegatives<T: SignedInteger>(arr: [T], by isSorted: (T, T) -> Bool) -> [T] {
+    var arr = arr
+    var madeSwap = false
+    repeat {
+        madeSwap = false
+        print(arr)
+        for i in 0...arr.count - 1 {
+            if arr[i] < 0 {
+                continue
+            }
+            for j in i..<arr.count - 1 {
+                if arr[j+1] < 0 {
+                    continue
+                } else if !isSorted(arr[i],arr[j+1]) && arr[i] != arr[j+1] {
+                    arr.swapAt(i, j+1)
+                    madeSwap = true
+                }
+                break
+            }
+            
+        }
+    } while madeSwap
     return arr
 }
 
